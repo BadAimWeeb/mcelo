e642ecfe7306ed34041f2d4cd076365936b74288
BadAimWeeb <badaimweeb@protonmail.com>
2025-02-20 07:57:21 -0600

    scaffold

---
+++ plugin/src/main/java/me/badaimweeb/mcelo/Database.java
@@ -0,0 +1,10 @@
+package me.badaimweeb.mcelo;
+
+import java.io.PrintWriter;
+import java.util.Properties;
+
+public class Database {
+    public Database() {
+
+    }
+}
---
+++ plugin/src/main/java/me/badaimweeb/mcelo/MCElo.java
@@ -0,0 +1,12 @@
+package me.badaimweeb.mcelo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MCElo extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        this.saveDefaultConfig();
+    }
+    @Override
+    public void onDisable() {}
+}
